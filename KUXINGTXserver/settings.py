c61ae63567511d57b52f77a55af543a0daaf0d25
2430130350 <498478105@qq.com>
2019-05-29 20:45:36 +0800

    历史遗留

--- KUXINGTXserver/settings.py
+++ KUXINGTXserver/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'ru11t*kgy%i2m+^ej4em4uwp+cji-3t&q_yu)a@4h9hs_y)ti&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "10.120.175.14",
+    "192.168.137.1",
+]
 
 
 # Application definition
@@ -43,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',

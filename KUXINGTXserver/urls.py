41121c45697638ee464e0023a740d64b6886f44b
Li <736872721@qq.com>
2019-05-30 15:52:26 +0800

    可爱滴文文瑾子

--- KUXINGTXserver/urls.py
+++ KUXINGTXserver/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from server import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', views.loginPost, name='loginPost'),
 ]
